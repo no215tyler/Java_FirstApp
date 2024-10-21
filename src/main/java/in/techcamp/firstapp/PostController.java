52b1a86530864e08b4698b8438ae1abfb5a309a0
no215tyler <no.215.tyler@gmail.com>
2024-10-21 21:58:57 +0900

    update: PostコントローラーとPostエンティティの更新

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -1,15 +1,30 @@
 package in.techcamp.firstapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+    private final PostRepository postRepository;
+
     @GetMapping("/hello")
     public String showHello(Model model){
-        var sampleText = "サンプルテキスト";
+        var sampleText = (21.1 + 20.5) / 2;
         model.addAttribute("sampleText", sampleText);
         return "hello";
+    }
+
+    @GetMapping
+    public ModelAndView showList(ModelAndView mav) {
+        var postList = postRepository.findAll();
+        mav.setViewName("index");
+        mav.addObject("postList", postList);
+        return mav;
     }
 }
