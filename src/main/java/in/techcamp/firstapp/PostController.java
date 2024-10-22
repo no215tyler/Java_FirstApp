e33f3a714ea5373c5cd13ae2025ec1fa875254e4
no215tyler <no.215.tyler@gmail.com>
2024-10-22 23:04:58 +0900

    add: 投稿ページの実装

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -25,6 +26,12 @@ public class PostController {
         var postList = postRepository.findAll();
         mav.setViewName("index");
         mav.addObject("postList", postList);
+        return mav;
+    }
+
+    @GetMapping("/postForm")
+    public ModelAndView showPostForm(@ModelAttribute("postForm") PostForm form, ModelAndView mav) {
+        mav.setViewName("postForm");
         return mav;
     }
 }
---
+++ src/main/java/in/techcamp/firstapp/PostForm.java
@@ -0,0 +1,7 @@
+package in.techcamp.firstapp;
+
+import lombok.Data;
+@Data
+public class PostForm {
+    private String memo;
+}
